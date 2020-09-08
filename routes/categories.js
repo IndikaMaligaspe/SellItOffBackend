e9fbeec4d98fe6d82d27a60442eb0c6f1ccec746
IndikaMaligaspe <k.indika.maligaspe@.gmail.com>
2020-09-08 22:29:18 +0530

    added all DAO to mongo

--- routes/categories.js
+++ routes/categories.js
@@ -1,10 +1,42 @@
 const express = require("express");
 const router = express.Router();
-const categoriesStore = require("../store/categories");
 
-router.get("/", (req, res) => {
-  const categories = categoriesStore.getCategories();
+const Joi = require("joi");
+const validateWith = require("../middleware/validation")
+
+const categoriesStore = require("../store/categories");
+const validation = require("../middleware/validation");
+
+router.get("/", async (req, res) => {
+
+  let categories = await categoriesStore.getCategories();
   res.send(categories);
 });
 
+const schema = {
+  id:Joi.string().required(),
+  name:Joi.string().required(),
+  icon: Joi.string().required(),
+  backgroundColor: Joi.string().required(),
+  color:Joi.string().required(),
+}
+router.post("/", 
+            [
+              validateWith(schema),
+            ] ,
+            async(req,res) =>{
+              let category = {
+                id:req.body.id,
+                name:req.body.name,
+                icon: req.body.icon,
+                backgroundColor: req.body.backgroundColor,
+                color:req.body.color,
+              }
+              try{
+                await categoriesStore.addCategory(category);
+                res.status(201).send(category);
+              }catch(error){
+                res.status(500).send(error);
+              }
+});
 module.exports = router;
--- store/categories.js
+++ store/categories.js
@@ -1,74 +1,115 @@
-const categories = [
-  {
-    id: 1,
-    name: "Furniture",
-    icon: "floor-lamp",
-    backgroundColor: "#fc5c65",
-    color: "white"
-  },
-  {
-    id: 2,
-    name: "Cars",
-    icon: "car",
-    backgroundColor: "#fd9644",
-    color: "white"
-  },
-  {
-    id: 3,
-    name: "Cameras",
-    icon: "camera",
-    backgroundColor: "#fed330",
-    color: "white"
-  },
-  {
-    id: 4,
-    name: "Games",
-    icon: "cards",
-    backgroundColor: "#26de81",
-    color: "white"
-  },
-  {
-    id: 5,
-    name: "Clothing",
-    icon: "shoe-heel",
-    backgroundColor: "#2bcbba",
-    color: "white"
-  },
-  {
-    id: 6,
-    name: "Sports",
-    icon: "basketball",
-    backgroundColor: "#45aaf2",
-    color: "white"
-  },
-  {
-    id: 7,
-    name: "Movies & Music",
-    icon: "headphones",
-    backgroundColor: "#4b7bec",
-    color: "white"
-  },
-  {
-    id: 8,
-    name: "Books",
-    icon: "book-open-variant",
-    backgroundColor: "#a55eea",
-    color: "white"
-  },
-  {
-    id: 9,
-    name: "Other",
-    icon: "application",
-    backgroundColor: "#778ca3",
-    color: "white"
+
+const mongoose = require("mongoose")
+
+const CategorySchema = mongoose.Schema({
+  id:String,
+  name:String,
+  icon: String,
+  backgroundColor: String,
+  color:String,
+});
+
+const model = mongoose.model('Category', CategorySchema);
+
+const getCategories = async () => {
+  let categories = []
+  try{
+    categories =  await model.find({}).lean();
+  }catch(error){
+    console.log(error);
   }
-];
+  return categories;
+}
 
-const getCategories = () => categories;
+const getCategory =async  (id) => {
+  let category = {}
+  try{
+    category =  await model.findById(id).lean();
+  }catch(error){
+    console.log(error);
+  }
+  return category;
+}
 
-const getCategory = id => categories.find(c => c.id === id);
-
+const addCategory = async (category) => {
+  let categoryObj = new model({
+    id:category.id,
+    name:category.name,
+    icon: category.icon,
+    backgroundColor: category.backgroundColor,
+    color:category.color,
+  });
+  return (await categoryObj.save());
+}
 module.exports = {
   getCategories,
-  getCategory
+  getCategory,
+  addCategory,
 };
+
+
+// const categories = [
+//   {
+//     id: 1,
+//     name: "Furniture",
+//     icon: "floor-lamp",
+//     backgroundColor: "#fc5c65",
+//     color: "white"
+//   },
+//   {
+//     id: 2,
+//     name: "Cars",
+//     icon: "car",
+//     backgroundColor: "#fd9644",
+//     color: "white"
+//   },
+//   {
+//     id: 3,
+//     name: "Cameras",
+//     icon: "camera",
+//     backgroundColor: "#fed330",
+//     color: "white"
+//   },
+//   {
+//     id: 4,
+//     name: "Games",
+//     icon: "cards",
+//     backgroundColor: "#26de81",
+//     color: "white"
+//   },
+//   {
+//     id: 5,
+//     name: "Clothing",
+//     icon: "shoe-heel",
+//     backgroundColor: "#2bcbba",
+//     color: "white"
+//   },
+//   {
+//     id: 6,
+//     name: "Sports",
+//     icon: "basketball",
+//     backgroundColor: "#45aaf2",
+//     color: "white"
+//   },
+//   {
+//     id: 7,
+//     name: "Movies & Music",
+//     icon: "headphones",
+//     backgroundColor: "#4b7bec",
+//     color: "white"
+//   },
+//   {
+//     id: 8,
+//     name: "Books",
+//     icon: "book-open-variant",
+//     backgroundColor: "#a55eea",
+//     color: "white"
+//   },
+//   {
+//     id: 9,
+//     name: "Other",
+//     icon: "application",
+//     backgroundColor: "#778ca3",
+//     color: "white"
+//   }
+// ];
